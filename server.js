052bd72fa67dc626ab00f4f9d75ca56440944250
MattRodigheri <mrodigheri@gmail.com>
2018-09-18 14:16:11 -0700

    sidebar css

--- server.js
+++ server.js
@@ -5,11 +5,19 @@ const app = express();
 const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 const BodyParser = require('body-parser');
-
-mongoose.connect('mongodb://localhost/fec');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
 
 app.use(morgan('dev'));
 app.use('/:id', express.static(path.join(__dirname, 'public')));
+app.use(BodyParser.urlencoded({extended: true}));
+app.use(BodyParser.json());
+app.use('/:id', express.static(__dirname + './../public/dist'));
+app.use('/:rest_id', express.static(__dirname + './../public/'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Reviews Server Info:
+mongoose.connect('mongodb://localhost/fec');
 
 const reviewSchema = new mongoose.Schema({
   reviewId: Number,
@@ -44,16 +52,36 @@ const getRestaurantReviews = (id, callback) => {
   });
 };
 
-
-// const db = require('./../database/');
-
-app.use(BodyParser.urlencoded({extended: true}));
-app.use(BodyParser.json());
-app.use('/:id', express.static(__dirname + './../public/dist'));
-
 app.get('/reviews/id/:id', (req, res) => {
   getRestaurantReviews(req.params.id, (reviews) => {
     res.send(reviews);
+  });
+});
+
+//Photos Server Info:
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'photos'
+});
+
+var getPhotos = function(restId, callback) {
+  connection.query(`SELECT * FROM photos WHERE rest_id = ${restId}`, function (err, rows, fields) {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, rows);
+    }
+  });
+};
+
+app.get('/photos/:rest_id', function(req, res) {
+  getPhotos(req.params.rest_id, function(err, data) {
+    if (err) {
+      res.sendStatus(503);
+    } else {
+      res.send(data);
+    }
   });
 });
 
